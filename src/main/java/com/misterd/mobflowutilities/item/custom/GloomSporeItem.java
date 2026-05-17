f06ad8261ae0db0a08b32a4b2c214eda23a2ca87
deonjonker123 <deon.jonker@hotmail.com>
2026-05-17 23:39:50 +0200

    v3.1.10.2-MC-1.21.1: Fixed dark glass connected texture when using Sodium/Iris

--- src/main/java/com/misterd/mobflowutilities/item/custom/GloomSporeItem.java
+++ src/main/java/com/misterd/mobflowutilities/item/custom/GloomSporeItem.java
@@ -3,6 +3,8 @@ package com.misterd.mobflowutilities.item.custom;
 import com.misterd.mobflowutilities.block.MFUBlocks;
 import com.misterd.mobflowutilities.config.Config;
 import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
@@ -41,6 +43,7 @@ public class GloomSporeItem extends Item {
 
                 if (isConvertibleBlock(targetState)) {
                     level.setBlock(targetPos, MFUBlocks.DARK_DIRT.get().defaultBlockState(), 3);
+                    level.levelEvent(2001, targetPos, Block.getId(targetState));
                     blocksConverted++;
                 }
             }
@@ -48,6 +51,7 @@ public class GloomSporeItem extends Item {
 
         if (blocksConverted > 0) {
             stack.shrink(1);
+            level.playSound(null, clickedPos, SoundEvents.SOUL_SAND_PLACE, SoundSource.BLOCKS, 2.0F, 1.0F);
             return InteractionResult.SUCCESS;
         }
 
